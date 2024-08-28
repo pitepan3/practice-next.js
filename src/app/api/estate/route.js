7ed94bbf50babc7b9cb99ead6cd7142a60b2973a
donghoonchoi <pitepan3@naver.com>
2024-08-28 19:46:19 +0900

    estate폴더 분리

--- src/app/api/estate/route.js
+++ src/app/api/estate/route.js
@@ -1,25 +1,39 @@
 import { NextResponse } from 'next/server';
-import { REGION_CODES } from '../data/region_codes.js';
+import getRealEstateTradingArea from './getRealEstateTradingArea';
+import getRealEstateTradingCount from './getRealEstateTradingCount';
+import getRealEstateTradingAreaResidence from './getRealEstateTradingAreaResidence';
+import getRealEstateTradingCountResidence from './getRealEstateTradingCountResidence';
+import getRealEstateTradingAreaBuildType from './getRealEstateTradingAreaBuildType';
+import getRealEstateTradingCountBuildType from './getRealEstateTradingCountBuildType';
+import getRealEstateTradingAreaDealer from './getRealEstateTradingAreaDealer';
+import getRealEstateTradingCountDealer from './getRealEstateTradingCountDealer';
+// 필요한 추가 API 호출 파일들을 import
 
 export async function GET(request) {
-  const API_KEY = process.env.NEXT_PUBLIC_PUBLIC_RENTAL_HOUSE_KEY;
-  const requests = REGION_CODES.map((regionCode) => {
-    const apiUrl = `https://api.odcloud.kr/api/RealEstateTradingSvc/v1/getRealEstateTradingArea?page=1&perPage=10&cond[RESEARCH_DATE::LTE]=202112&cond[RESEARCH_DATE::GTE]=202101&cond[REGION_CD::EQ]=${regionCode}&cond[DEAL_OBJ::EQ]=01&serviceKey=${API_KEY}`;
-    return fetch(apiUrl).then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      return response.json();
-    });
-  });
-
   try {
-    const results = await Promise.all(requests);
-    // 결과를 하나의 배열로 합칩니다.
-    const allData = results.flatMap((result) => result.data);
-    return NextResponse.json({ currentCount: allData.length, data: allData });
+    const [areaData, countData, areaResidenceData, CountResidenceData, AreaBuildTypeData, CountBuildTypeData, AreaDealerData, CountDealerData] = await Promise.all([
+      getRealEstateTradingArea(),
+      getRealEstateTradingCount(),
+      getRealEstateTradingAreaResidence(),
+      getRealEstateTradingCountResidence(),
+      getRealEstateTradingAreaBuildType(),
+      getRealEstateTradingCountBuildType(),
+      getRealEstateTradingAreaDealer(),
+      getRealEstateTradingCountDealer(),
+    ]);
+
+    return NextResponse.json({
+      areaData: areaData.data, // { data: ... } 형태로 반환된 데이터를 사용
+      countData: countData.data, // { data: ... } 형태로 반환된 데이터를 사용
+      areaResidenceData: areaResidenceData.data,
+      CountResidenceData: CountResidenceData.data,
+      AreaBuildTypeData: AreaBuildTypeData.data,
+      CountBuildTypeData: CountBuildTypeData.data,
+      AreaDealerData: AreaDealerData.data,
+      CountDealerData: CountDealerData.data,
+    });
   } catch (error) {
-    console.error('Error fetching data:', error);
+    console.error('Error fetching data:', error.message);
     return NextResponse.error();
   }
 }
